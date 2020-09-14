10507317ee505d0d2b0c936c894d3e2a22dad446
Hertzy Tambwe <53703925+HertzyTambwe@users.noreply.github.com>
2020-09-14 19:07:27 +0200

    Add project files.

---
+++ TravelRecordApp/TravelRecordApp/Helpers/Constans.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelRecordApp.Helpers
+{
+    public class Constans
+    {
+        public const string VENUE_SEARCH = "https://api.foursquare.com/v2/venues/search?ll={0},{1}&client_id={2}&client_secret={3}&v={4}";
+        public const string CLIENT_ID = "EI5XUB02KKIUM4VAP4QG4SZ1VK5U11NLTGISP50KHY4HKLSH";
+        public const string CLIENT_SECRET = "ZUI4F20MJJ5QBQEVU0AIQQD1OWBMEI5MEDXDVT5IMQ5WSGGG";
+    }
+}
---
+++ TravelRecordApp/TravelRecordApp/NewTravelPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Geolocator;
+using TravelRecordApp.Logic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecordApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewTravelPage : ContentPage
+    {
+        public NewTravelPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var locator = CrossGeolocator.Current;
+            var position = await locator.GetPositionAsync();
+            var venues = await VenueLogic.GetVenues(position.Latitude, position.Longitude);
+
+            venueListView.ItemsSource = venues;
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
